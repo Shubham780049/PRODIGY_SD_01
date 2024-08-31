6124a0aa51e9952b22a567c544c6a892210708c5
Shubham Tiwari <160890896+Shubham780049@users.noreply.github.com>
2024-08-31 12:21:38 +0530

    Add files via upload
    This is the very basic of web scraping...

---
+++ scraper.py
@@ -0,0 +1,12 @@
+import requests
+
+from bs4 import BeautifulSoup
+
+req = requests.get("https://www.instagram.com/")
+
+soup=BeautifulSoup(req.content,"html.parser")
+
+res= soup.title
+
+
+print(res.get_text())
